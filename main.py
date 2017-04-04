d756e9eff997f882cb14addfe8675c366139c9c7
SanderRonde <awsdfgvhbjn@gmail.com>
2017-04-04 02:48:32 +0200

    should now properly make model for each user

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import pandas as pd
-import datetime
+import tensorflow as tf
+
+lstm_size = 100
+chunksize = 2 ** 8
+
+models = dict()
+
+def getRowUser(row):
+    return row[3]
+
+def generateTFModel():
+    lstm = tf.contrib.rnn.BasicLSTMCell()
+    state = tf.zeros([batch_size, lstm.state_size])
+    return [lstm, state]
+
+def assertModelInDict(user):
+    if !(user in models):
+        models[user] = generateTFModel()
+
+def doIteration(user, data):
+    models[user] = models[user][0](data, models[user][1])
+
+def handleRow(row):
+    user = getRowUser(row)
+    assertModelInDict(user)
+    doIteration(user, row)
 
 def iterate():
-    chunksize = 10 ** 6
     for chunk in pd.read_hdf('/data/s1481096/LosAlamos/data/auth_small.h5', 'auth_small',
             chunksize=chunksize):
         for name, group in chunk.groupby(
                 [chunk.index, pd.TimeGrouper(freq='Min')]):
             for row in group.itertuples():
-                print(row)
+                handleRow(row)            
 
 def main():
     iterate()
